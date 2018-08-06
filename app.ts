7625e8caa9a4ea1d5b47f6a907f1fec9ed295d9c
coderming <coderming@foxmail.com>
2018-08-06 19:48:10 +0800

    搭建简单的server

--- app.ts
+++ app.ts
@@ -1,3 +1,20 @@
-import xx from './src/index'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as Koa from 'koa'
+import * as KoaRouter from 'koa-router'
+import * as logger from 'koa-logger'
 
-xx()
+const app = new Koa()
+const router = new KoaRouter()
+
+app.use(logger())
+app.use(router.routes())
+
+app.use(ctx => {
+  ctx.status = 200
+  ctx.body = {
+    msg: 'hello world!'
+  }
+})
+
+app.listen(3000)
