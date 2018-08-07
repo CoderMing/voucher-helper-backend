22f81595234c6b2365325339842e4fc96da84cae
coderming <coderming@foxmail.com>
2018-08-07 10:41:04 +0800

    fix typo

--- app.ts
+++ app.ts
@@ -4,18 +4,17 @@ import router from './routes/index'
 
 const app = new Koa()
 
-
 app.use(logger())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 app.use(router.routes())
 
-// app.use(ctx => {
-//   ctx.status = 200
-//   ctx.body = {
-//     msg: 'hello world!'
-//   }
-// })
+app.use(ctx => {
+  ctx.status = 404
+  ctx.body = {
+    msg: 'not found'
+  }
+})
 
 app.listen(3000)
--- routes/clover.ts
+++ routes/clover.ts
@@ -5,7 +5,6 @@ const router = new KoaRouter()
 import { RouterFormat } from './index'
 
 router.get('/', async ctx => {
-  console.log('123')
   ctx.status = 200
   ctx.body = '12313'
 })
